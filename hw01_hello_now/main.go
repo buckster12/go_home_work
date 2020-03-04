ee51620f5dceae38123029b1229e77b4b68dd95d
buckster12 <roboks@mail.ru>
2020-03-04 11:39:36 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	currentTime := time.Now().UTC()
+	ntpTime, _ := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	dateFormat := "2006-01-02 15:04:05 +0000 UTC"
+	fmt.Printf("current time: %s\n", currentTime.Format(dateFormat))
+	fmt.Printf("exact time: %s\n", ntpTime.UTC().Format(dateFormat))
 }
