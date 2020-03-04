f7242793442c258023fbbd6f1ae9046df793c2d6
buckster12 <roboks@mail.ru>
2020-03-04 12:00:43 +0300

    log error and exit added

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/beevik/ntp"
@@ -9,7 +10,10 @@ import (
 
 func main() {
 	currentTime := time.Now().UTC()
-	ntpTime, _ := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
 	dateFormat := "2006-01-02 15:04:05 +0000 UTC"
 	fmt.Printf("current time: %s\n", currentTime.Format(dateFormat))
 	fmt.Printf("exact time: %s\n", ntpTime.UTC().Format(dateFormat))
